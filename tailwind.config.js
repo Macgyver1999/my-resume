f9a0016770868f88eba93fa24b97abb917ae3a1c
Nattawat <mr.nattawat.265314@gmail.com>
2024-11-04 14:34:55 +0700

    update 4 nov

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primaryTitle: '#D9B384',
+                primaryContent: '#F21B07',
+                primarySubcontent: '#D91807',
+                primaryBase: '##04BF9D',
+                primaryAccent: '#D96B52',
+                primaryBg: '#1B2624',
+            },
+        },
     },
     plugins: [],
 }
